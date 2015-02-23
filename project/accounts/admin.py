cff06c07fd64b14c8782e3f9029e8b9d2fff5fa8
greenteamer <greenteamer@bk.ru>
2015-02-23 19:11:27 +0600

    add account application

---
+++ project/accounts/admin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from project.accounts.models import BaseUserInfo, UserProfile
+# Register your models here.
+
+# class UserInline(admin.StackedInline):
+#     """Вывод заказов списком"""
+#     model = User
+#     extra = 0
+
+# class UserProfileAdmin(admin.ModelAdmin):
+#     model = UserProfile
+#     inlines = [UserInline]
+
+admin.site.register(UserProfile)
+
