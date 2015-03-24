e0f52820210d821df042b9052c9a5f9ffc3589c0
ghost <2100636@mail.ru>
2015-03-24 14:39:55 +0600

    ///

--- project/pristroy/forms.py
+++ project/pristroy/forms.py
@@ -10,7 +10,7 @@ class ProductForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs = {'class': 'form-control'}
+            self.fields[field].widget.attrs = {'placeholder': self.fields[field].label, 'class': 'form-control'}
         # пример фукнции map
         # def f(field):
         #     self.fields[field].widget.attrs = {'class': 'form-control'}
