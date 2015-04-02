ba6bf98f2aa21d3606ec3a365c9fc061d27825e4
2100636 <2100636@mail.ru>
2015-04-02 13:53:57 +0600

    работа с изображениями в пристрое.

--- project/pristroy/forms.py
+++ project/pristroy/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.forms import ModelForm, Form
-from models import Product
+from models import Product, ProductImages
 
 
 class ProductForm(ModelForm):
@@ -15,5 +15,22 @@ class ProductForm(ModelForm):
         # def f(field):
         #     self.fields[field].widget.attrs = {'class': 'form-control'}
         # map(f, self.fields)
+
+
+
+class ProductImagesForm(ModelForm):
+    class Meta:
+        model = ProductImages
+        exclude = ('p_image_product',)
+    def __init__(self, *args, **kwargs):
+        super(ProductImagesForm, self).__init__(*args, **kwargs)
+        self.fields['image'].widget.attrs = {'class': 'btn btn-block btn-default btn-sm', 'multiple': 'multiple'}  #
+    def save(self, product_id):
+        obj = super(ProductImagesForm, self).save(commit=False)
+        obj.p_image_product = Product.objects.get(id=product_id)
+        obj.save()
+        return obj
+
+
 
 
