ade7bf4e5f674f78fe9b4e787a071c41194e039e
greenteamer <greenteamer@bk.ru>
2015-05-26 12:21:24 +0600

    небольшие поправки товара

---
+++ static/app/catalog/main_catalog.js
@@ -0,0 +1,18 @@
+(function (window){
+
+	'use strict';
+
+	var ProductApp = {
+		Models: {},
+		Collections: {},
+		Views: {},
+		Constants: {
+			posts_api: '/api/v1/products/',
+			// apiKey: '4cLJd234l243ljradf3Iy43lOU23fsdlf3' // пока не понятно зачем это, но догадываюсь =) )
+		}		
+	};
+
+	
+	window.ProductApp = ProductApp;
+
+})(window);
