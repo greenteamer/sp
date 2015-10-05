408d296ea8498278987f16e6356acf3cee748a77
greenteamer <greenteamer@bk.ru>
2015-10-05 13:59:06 +0600

    переезд на сборщик gulp

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,36 +1,53 @@
-// переменная path - настройки констант для путей и названий файлов
-// 4 таска для билдинга 4 файлов для главной страницы, категорий, товаров и закупок
-// последний таск запускает их все
 var gulp = require('gulp'),
 		concatCss = require('gulp-concat-css'),
 		minifyCss = require('gulp-minify-css'),
 		rename = require("gulp-rename"),
+		cjsx = require('gulp-cjsx'),
 		uglify = require('gulp-uglify'),
 		source = require('vinyl-source-stream'),
 		browserify = require('browserify'),
 		reactify = require('reactify'),
+		coffeeReactify = require('coffee-reactify'),
 		watchify = require('watchify'),
 		streamify = require('gulp-streamify');
 
 
+// gulp for css
+gulp.task('default', function(){
+	gulp.src('css/test/*.css')
+		.pipe(concatCss('bundle.css'))
+		.pipe(minifyCss())
+		.pipe(rename('bundle.min.css'))
+		.pipe(gulp.dest('css/'));		
+});
+
+
+// переменная path - настройки констант для путей и названий файлов
+// 4 таска для билдинга 4 файлов для главной страницы, категорий, товаров и закупок
+// последний таск запускает их все
+// Умеет работать с COFFEESCRIPT
 var path = {
-	MINIFIED_OUT: 'build.min.js',
 	DEST: 'client',
 	DEST_BUILD: 'client/build',
+	PROD_DEST_BUILD: 'client/build/production',
 	DEST_SRC: 'client/dev',
 	index: {
+		MINIFIED_OUT: 'index-build.min.js',		
 		OUT: 'index-build.js',
 		ENTRY_POINT: './client/dev/index/main.jsx'
 	},
 	category: {
+		MINIFIED_OUT: 'index-category.min.js',		
 		OUT: 'category-build.js',
 		ENTRY_POINT: './client/dev/category/main.jsx'
 	},
 	product: {
+		MINIFIED_OUT: 'index-product.min.js',		
 		OUT: 'product-build.js',
 		ENTRY_POINT: './client/dev/product/main.jsx'	
 	},
 	purchase: {
+		MINIFIED_OUT: 'index-purchase.min.js',		
 		OUT: 'purchase-build.js',
 		ENTRY_POINT: './client/dev/purchase/main.jsx'
 	}	
@@ -41,7 +58,7 @@ var path = {
 gulp.task('index-watch', function(){
 	var watcher = watchify(browserify({
 		entries: [path.index.ENTRY_POINT],
-		transform: [reactify],
+		transform: [coffeeReactify],
 		debug: true,
 		cache:{}, packageCache: {}, fullPath: true
 	}));
@@ -62,7 +79,7 @@ gulp.task('index-watch', function(){
 gulp.task('category-watch', function(){
 	var watcher = watchify(browserify({
 		entries: [path.category.ENTRY_POINT],
-		transform: [reactify],
+		transform: [coffeeReactify],
 		debug: true,
 		cache:{}, packageCache: {}, fullPath: true
 	}));
@@ -83,7 +100,7 @@ gulp.task('category-watch', function(){
 gulp.task('product-watch', function(){
 	var watcher = watchify(browserify({
 		entries: [path.product.ENTRY_POINT],
-		transform: [reactify],
+		transform: [coffeeReactify],
 		debug: true,
 		cache:{}, packageCache: {}, fullPath: true
 	}));
@@ -104,7 +121,7 @@ gulp.task('product-watch', function(){
 gulp.task('purchase-watch', function(){
 	var watcher = watchify(browserify({
 		entries: [path.purchase.ENTRY_POINT],
-		transform: [reactify],
+		transform: [coffeeReactify],
 		debug: true,
 		cache:{}, packageCache: {}, fullPath: true
 	}));
@@ -121,22 +138,61 @@ gulp.task('purchase-watch', function(){
 });
 
 
-// gulp for css
-gulp.task('default', function(){
-	gulp.src('css/test/*.css')
-		.pipe(concatCss('bundle.css'))
-		.pipe(minifyCss())
-		.pipe(rename('bundle.min.css'))
-		.pipe(gulp.dest('css/'));		
+// PRODUCTION
+// собираем файлы для продакшена
+gulp.task('index-build', function(){
+  browserify({
+    entries: [path.index.ENTRY_POINT],
+    transform: [coffeeReactify],
+  })
+    .bundle()
+    .pipe(source(path.index.MINIFIED_OUT))    
+    .pipe(streamify(uglify({sequences: true})))    
+    .pipe(gulp.dest(path.PROD_DEST_BUILD))
 });
+
+
+gulp.task('category-build', function(){
+  browserify({
+    entries: [path.category.ENTRY_POINT],
+    transform: [coffeeReactify],
+  })
+    .bundle()
+    .pipe(source(path.category.MINIFIED_OUT))    
+    .pipe(streamify(uglify({sequences: true})))    
+    .pipe(gulp.dest(path.PROD_DEST_BUILD))
+});
+
+
+gulp.task('product-build', function(){
+  browserify({
+    entries: [path.product.ENTRY_POINT],
+    transform: [coffeeReactify],
+  })
+    .bundle()
+    .pipe(source(path.product.MINIFIED_OUT))    
+    .pipe(streamify(uglify({sequences: true})))    
+    .pipe(gulp.dest(path.PROD_DEST_BUILD))
+});
+
+
+gulp.task('purchase-build', function(){
+  browserify({
+    entries: [path.purchase.ENTRY_POINT],
+    transform: [coffeeReactify],
+  })
+    .bundle()
+    .pipe(source(path.purchase.MINIFIED_OUT))    
+    .pipe(streamify(uglify({sequences: true})))    
+    .pipe(gulp.dest(path.PROD_DEST_BUILD))
+});
+
 
 gulp.task('watch-css', function(){
 	gulp.watch('css/test/*.css', ['default']);
 });
 
-
 gulp.task('default', ['index-watch', 'category-watch', 'product-watch', 'purchase-watch']);
-
-
+gulp.task('production', ['index-build', 'category-build', 'product-build', 'purchase-build']);
 
 
